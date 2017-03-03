bf2adfa6dc9165cf2088325f36ac3d6b4f1ca952
Slavko Novak <slavko.novak.esen@gmail.com>
2017-03-03 11:39:05 +0100

    -

---
+++ Release/Command.cpp.d
@@ -0,0 +1,15 @@
+D:\Users\snovak\AppData\Local\Temp\VMBuilds\GDC\nano_atmega328\Release\Command.cpp.o: \
+ D:\Users\snovak\AppData\Local\Temp\VMBuilds\GDC\nano_atmega328\Release\Command.cpp \
+ D:\Users\snovak\AppData\Local\Temp\VMBuilds\GDC\nano_atmega328\Release\Command.h \
+ D:\Embed\arduino-1.6.5\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Embed\arduino-1.6.5\hardware\arduino\avr\cores\arduino/binary.h \
+ D:\Embed\arduino-1.6.5\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ D:\Embed\arduino-1.6.5\hardware\arduino\avr\cores\arduino/WString.h \
+ D:\Embed\arduino-1.6.5\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ D:\Embed\arduino-1.6.5\hardware\arduino\avr\cores\arduino/Stream.h \
+ D:\Embed\arduino-1.6.5\hardware\arduino\avr\cores\arduino/Print.h \
+ D:\Embed\arduino-1.6.5\hardware\arduino\avr\cores\arduino/Printable.h \
+ D:\Embed\arduino-1.6.5\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ D:\Embed\arduino-1.6.5\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Embed\arduino-1.6.5\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ D:\Embed\arduino-1.6.5\hardware\arduino\avr\variants\eightanaloginputs/../standard/pins_arduino.h
---
+++ Release/uHDL.cpp.d
@@ -0,0 +1,3 @@
+D:\Users\snovak\AppData\Local\Temp\VMBuilds\GDC\nano_atmega328\Release\uHDL.cpp.o: \
+ D:\Users\snovak\AppData\Local\Temp\VMBuilds\GDC\nano_atmega328\Release\uHDL.cpp \
+ D:\Users\snovak\AppData\Local\Temp\VMBuilds\GDC\nano_atmega328\Release\uHDL.h
